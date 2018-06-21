8bceb3c9f2399c8953b66fb005f80db36cecb3c5
asathoor <asathoor@gmail.com>
2018-06-21 20:51:26 +0200

    tidy myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -9,12 +9,12 @@ var $j = jQuery.noConflict();
 
 /**
  * Run jQuery in a "no-conflict" way
+ * document-ready-function
  **/
-
 $j(function(){
 
     console.log('Cheers from jQuery too!')
 
     // ... add your code here ...
 
-}); // ends jQuery (don't delete this line!)
+}); // ends jQuery on-document-ready (don't delete this line!)
