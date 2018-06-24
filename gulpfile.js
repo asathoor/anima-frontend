ec6ca38b156291b3e38b79123bb3e0cb338a886b
asathoor <asathoor@gmail.com>
2018-06-24 19:48:02 +0200

    gulpfile tidy

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
+// imports
 var gulp = require('gulp');
 var sass = require('gulp-sass')
-
-// test
-gulp.task('hello', function(){
-  console.log('gulping now');
-});
 
 // sass
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('.'));
-    console.log('pipe to dest');
 });
 
 // sass watch
